10b87f0aac607645f2574c33f3b6d1ab36be0b80
Yash Arya <yash72614@gmail.com>
2025-03-01 01:56:05 +0530

    Switched to HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/e-commerce-website/', // Make sure this matches your GitHub repo name
+});
